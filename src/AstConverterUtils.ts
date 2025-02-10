62dca78e7cae192d86ca1a8b8554566e3651f48b
João Bispo <jbispo@fe.up.pt>
2025-02-10 17:22:13 +0000

    Adds AstConverterUtils.normalizeFilepath(), version 1.0.6

--- src/AstConverterUtils.ts
+++ src/AstConverterUtils.ts
@@ -5,45 +5,47 @@
 
 /**
  * @brief Adds indentation to the given code, with the exception of the first line.
- * 
+ *
  * @param code Code
- * @param indentation The indentation to use 
+ * @param indentation The indentation to use
  * @returns The indented code
  */
 const addIdentation = (code: string, indentation: number): string => {
-  return code.split('\n').map((line, i) => i > 0 ? '   '.repeat(indentation) + line : line).join('\n');
+  return code
+    .split("\n")
+    .map((line, i) => (i > 0 ? "   ".repeat(indentation) + line : line))
+    .join("\n");
 };
-
 
 /**
  * @brief Escapes the HTML special characters in the given text.
- * 
+ *
  * @param text Text to escape
  * @returns The escaped text
  */
 const escapeHtml = (text: string): string => {
   const specialCharMap: { [char: string]: string } = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
   };
-  
+
   return text.replace(/[&<>]/g, (match) => specialCharMap[match]);
 };
 
 /**
  * @brief Returns the opening and closing span tags with the given attributes.
- * 
+ *
  * @param attrs Attributes to add to the span tag
  * @returns An array with the opening and closing span tags
  */
 const getSpanTags = (...attrs: string[]): string[] => {
-  return [`<span ${attrs.join(' ')}>`, '</span>'];
+  return [`<span ${attrs.join(" ")}>`, "</span>"];
 };
 
 /**
  * @brief Returns the span tags for the code of the given node.
- * 
+ *
  * @param nodeId Node ID
  * @returns An array with the opening and closing span tags
  */
@@ -53,12 +55,34 @@ const getNodeCodeTags = (nodeId: string): string[] => {
 
 /**
  * @brief Returns the span tags for syntax highlighting of code of the given type..
- * 
+ *
  * @param type Code type
  * @returns An array with the opening and closing span tags
  */
-const getSyntaxHighlightTags = (type: 'comment' | 'keyword' | 'literal' | 'string' | 'type'): string[] => {
+const getSyntaxHighlightTags = (
+  type: "comment" | "keyword" | "literal" | "string" | "type"
+): string[] => {
   return getSpanTags(`class="${type}"`);
 };
 
-export { addIdentation, escapeHtml, getSpanTags, getNodeCodeTags, getSyntaxHighlightTags };
+/**
+ * @brief Normalizes a filepath, so that it only contains forward slashes.
+ * @param filepath
+ * @returns
+ */
+const normalizeFilepath = (filepath: string): string => {
+  if (filepath === undefined) {
+    return filepath;
+  }
+
+  return filepath.replace(/\\/g, "/");
+};
+
+export {
+  addIdentation,
+  escapeHtml,
+  getSpanTags,
+  getNodeCodeTags,
+  getSyntaxHighlightTags,
+  normalizeFilepath,
+};
